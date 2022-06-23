890c3742586467e8bccfcfcaf0e5dcf77d073d2d
anderson-mntn <88107035+anderson-mntn@users.noreply.github.com>
2022-06-23 19:55:44 -0300

    Add files via upload

--- chall-02.js
+++ chall-02.js
@@ -9,6 +9,10 @@
 //      }
 //  console.log(sumW([10,100,1000]));
  
+ let j = [1, 2]
+
+ let total = 0
+
  //Function way
  function myFunc (arr){
     let add = (a, b) => a + b;
@@ -19,7 +23,7 @@
  
 
 //Shorthand
-function myFunction(arr) {
+function myFunction( arr ) {
 return arr.reduce((acc, cur) => acc + cur, 0) / arr.length
 }
 
@@ -44,10 +48,35 @@ let func4 = arr =>{
       return b.length - a.length //we need get item's length, then sort it
    })[0]
 } 
-//Shorthand 
-//return arr.reduce((a, b) => a.length <= b.length ? b : a)
-
+//Shorthand : return arr.reduce((a, b) => a.length <= b.length ? b : a)
 console.log(func4(['help', 'me']));
 console.log(func4(['I', 'need', 'candy']));
 
 //-------------------------------
+const nomes = ["helena","miguel", "alice", "heitor",  "laura",  "manuela"
+]
+
+const pessoas = [nomes.shift(), ...nomes.slice(2, 4) ] 
+console.log(pessoas)// "helena"
+console.log(nomes); // "miguel", "alice", "heitor",  "laura",  "manuela"
+console.log(pessoas)// "helena", "alice", "heitor"
+
+//-------------------------------
+//Check if all array elements are equal
+//use every() -> return true iss the condition met for all array elements
+let func5 = arr => {
+    return arr.every(element =>{
+       if (element === arr[0]){
+          return true
+       }
+    } )
+   
+}
+//Shorthand
+//  return new Set(arr).size === 1
+//The Set object lets you store unique values of any type, whether primitive values or object references.
+
+console.log(func5([true, true, true, true]));
+console.log(func5(['test', 'test', 'test']));
+console.log(func5([1,1,1,2]));
+console.log(func5(['10',10,10,10]));
